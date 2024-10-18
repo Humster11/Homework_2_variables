6c54ac2251355937deb228320f47431e9c372372
daria.lapina <ms.humster11@gmail.com>
2024-10-19 00:44:06 +0300

    Выполненное ДЗ

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,52 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        dog = dog+4;
+        cat = cat+4;
+        paper = paper+4;
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend/7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog*10;
+        System.out.println(frog);
+        frog = frog/3.5;
+        System.out.println(frog);
+        frog = frog+4;
+        System.out.println(frog);
+
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+
+        var totalWeightBoxers = firstBoxerWeight + secondBoxerWeight;
+        System.out.println(totalWeightBoxers);
+
+        var diffWeightBoxers = firstBoxerWeight - secondBoxerWeight;
+        System.out.println(diffWeightBoxers);
+
+        var remainsBetweenTwoWeight = secondBoxerWeight%firstBoxerWeight;
+        System.out.println(remainsBetweenTwoWeight);
+
+        var workHours = 640;
+        var countEmployees = 640/8;
+        System.out.println("Всего работников в компании - "+countEmployees);
+        var incCountEmployees = countEmployees+94;
+        var incWorkHour = incCountEmployees*8;
+        System.out.println("Если в компании работает " +incCountEmployees+" человек, то всего "+incWorkHour+" часов работы может быть поделено между сотрудниками");
+
     }
 }
